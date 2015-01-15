2da8297154bce56150fbe905c5c9f580a87b590f
Isay Sosa <yasnet21@gmail.com>
2015-01-15 13:54:30 -0600

    Adding RecycleView and fragment list for books.

--- app/src/main/java/com/iszandro/library/BookListFragment.java
+++ app/src/main/java/com/iszandro/library/BookListFragment.java
@@ -1,9 +1,11 @@
 package com.iszandro.library;
 
 import android.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 
 /**
  * Created by Isay Sosa on 1/15/15.
  */
 public class BookListFragment extends Fragment {
+    private RecyclerView mRecyclerView;
 }
